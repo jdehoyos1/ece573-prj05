4f3af13504d11ee9c5ce95ad2a3ad57669170178
Jia <Jia Wang>
2024-05-16 16:26:33 -0500

    update table name

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t ece473-prj05-clients:v1 .
-kind load docker-image ece473-prj05-clients:v1
+docker build -t ece573-prj05-clients:v1 .
+kind load docker-image ece573-prj05-clients:v1
